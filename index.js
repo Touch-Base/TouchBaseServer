90ecfd81ec18349a463218724229983fe1c4e94a
Anthony Vigliotta <anthonyvigz@gmail.com>
2020-01-29 21:58:43 -0600

    setup knex for users

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
-    console.log(`\n** Welcome to Steveo! Running on port ${port} **\n`)
+    console.log(`\n** Welcome to TouchBase! Running on port ${port} **\n`)
 });
