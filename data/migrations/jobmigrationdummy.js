f545b2071ecac5425b4752d4904531927584a83d
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-06 12:57:15 -0500

    set up dummy for job table

---
+++ data/migrations/jobmigrationdummy.js
@@ -0,0 +1,22 @@
+exports.up = function(knex) {
+    return (knex.schema 
+        .createTable('job', tbl => {
+            tbl.increments('id');
+            tbl.string('position').notNullable(); // Position Name
+            tbl.string('company').notNullable(); // Company Name
+            tbl.date('appDate'); // Application Date
+            tbl.text('notes'); // Description of the job/interview notes
+            tbl.boolean('interview'); // Checks if if there is an interview offer
+            tbl
+              .integer('userId')
+              .unsigned()
+              .references('id')
+              .inTable('users'); // Matches the job to the user
+        })
+)};
+
+exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('job')
+  };
+      
