815fd2d3a84fe0b092db290c37fd538b9efd6c82
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-06 13:33:48 -0500

    added method column

--- data/migrations/jobmigrationdummy.js
+++ data/migrations/jobmigrationdummy.js
@@ -4,6 +4,8 @@ exports.up = function(knex) {
             tbl.increments('id');
             tbl.string('position').notNullable(); // Position Name
             tbl.string('company').notNullable(); // Company Name
+            tbl.string('link'); // Link to job description
+            tbl.enum('method', ['LinkedIn', 'Connection', 'Job Website', 'Company Site', 'Other']).notNullable()'
             tbl.date('appDate'); // Application Date
             tbl.text('notes'); // Description of the job/interview notes
             tbl.boolean('interview').defaultTo(false); // Checks if if there is an interview offer
