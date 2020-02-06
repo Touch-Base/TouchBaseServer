c8c45020f008fb90faebd1315c6f280650fa1675
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-06 16:08:34 -0500

    updated user login request

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -4,24 +4,16 @@ const bcrypt = require('bcryptjs');
 const Users = require('../helpers/usersModel.js');
 const jwt = require('jsonwebtoken');
 
-
-router.get('/', (req, res) => {
-  Users.find()
-    .then(users => {
-      res.status(201).json(users);
-    })
-    .catch(err => {
-      res.status(400).json({ message: err });
-    })
-})
+// USER REGISTER
 
 router.post('/register', (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); 
   user.password = hash;
 
-  /// adds user to the database
-  Users.add(user)
+  // Adds a new user
+  
+  Users.addUser(user)
     .then(saved => {
 
       /// creates token for the user
@@ -38,10 +30,15 @@ router.post('/register', (req, res) => {
     });
 });
 
+
+// USER LOGIN
+
 router.post('/login', (req, res) => {
   let { email, password } = req.body;
+  
+  // Finds user by email
 
-  Users.findBy({ email })
+  Users.getUserByEmail(email)
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
