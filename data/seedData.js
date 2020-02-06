f49acd6c5e2e0f5cf14a69846e293c2e0334892e
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-06 13:36:39 -0500

    setting up seeds for users and jobs

---
+++ data/seedData.js
@@ -0,0 +1,68 @@
+const faker = require('faker')
+
+module.exports = {
+
+  // seeds for users
+  
+  users: [
+    {
+      // master - 1
+      
+      firstname: 'Master',
+      lastname: 'User',
+      creationDate: '08/11/1992',
+      email: 'masterUser@gmail.com',
+      phone: '9788331813',
+      age: 29,
+      location: 'Boston, MA',
+      jobsTotal: 69,
+      connectionsTotal: 11,
+      summary: 'Hey my name is Mr. Master. Welcome to my profile.'
+      
+    },
+    {
+      // master - 2
+      
+      firstname: 'Sam',
+      lastname: 'Hynes',
+      creationDate: '08/12/1996',
+      email: 'samhynes@mac.com',
+      phone: '9788331814',
+      age: 18,
+      location: 'Lowell, MA',
+      jobsTotal: 29,
+      connectionsTotal: 12,
+      summary: 'Hey my name is Sam, I want to web develop!'
+    }
+  ],
+    
+    jobs: [
+      {
+        // job - 1
+        
+        position: 'Web Developer',
+        company: 'Arthrex',
+        appDate: '12/12/2009',
+        link: 'www.anthonyvigliotta.com',
+        method: 'Company Site',
+        notes: 'I really want to work here I hope they interview me!',
+        userId: 1
+      },
+      {
+        // job - 2
+        
+        position: 'Coder',
+        company: 'Athena Health',
+        appDate: '12/12/2229',
+        link: 'www.letsgetajob.com',
+        method: 'Other',
+        notes: 'I love Athena! They are in Waltham, MA',
+        userId: 1
+      }
+    ]
+        
+        
+        
+    
+    
+    
