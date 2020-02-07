6cfa9550d68f7882fd0aba9776c85bbc8b12496f
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-07 15:29:27 -0500

    added add job endpoint

---
+++ routers/jobsRouter.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const Jobs = require('../helpers/jobsModel');
+
+/// ADD JOB TO USER WITH USER ID
+
+router.post('/add', (req, res) => {
+  const job = req.body;
+  const { id } = req.body;
+  
+  Jobs.addJob(job, id)
+    .then(job => {
+      res.status(201).json({
+        newjob: job
+        message: 'New job added successfully!'
+        })
+      })
+    .catch(err => {
+      res.status(500).json({
+        message: err
+        })
+      });
+  });
