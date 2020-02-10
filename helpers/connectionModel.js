a0c6afce29811d9905fb2ca5304a3791ce5010db
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-10 12:07:59 -0500

    added add connection function

--- helpers/connectionModel.js
+++ helpers/connectionModel.js
@@ -5,10 +5,10 @@ const db = require('../data/db-config');  // imports database
 
 module.exports = {
   // Create
-//   addConnection,
+  addConnection,
   
   // Read
-//   getConnections,
+  getConnection,
   getAllConnections,
 //   getConnectionsByUser,
   
@@ -20,11 +20,34 @@ module.exports = {
 }
 
 // ---- CREATE ----
+ 
+// addConnection(connection, userId) - adds a connection to a specific user
 
+// ---- CREATE ----
 
+// addConnection(input) - inserts input to connections and returns results by userId
 
-
+async function addConnection(input, userId) {
+  const results = await db
+    .from('connections')
+    .returning('id')
+    .insert({ ...input, userId: userId })
+   
+   return getConnection(results[0])
+ }
+ 
 // ---- READ ----
+
+// getConnection() - returns results for a connection by id
+
+async function getConnection(id) {
+  const [connection] = await db
+    .from('connections')
+    .select('*')
+    .where({ id })
+  
+  return job || null;
+}
 
 // getAllConnections() - returns all results for connections
 
