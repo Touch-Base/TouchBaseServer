e1657cd4f1070cda9fd65e0801a9565b08c85608
Anthony Vigliotta <46168660+anthonyvigz@users.noreply.github.com>
2020-02-22 13:12:54 -0600

    console logging

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,11 +3,12 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
-  console.log(token)
+  console.log("we are looking for a token:", token)
 
   /// see if there is a token and check if it is valid
 
   if (token) {
+    console.log("here we are now")
     jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
         res.status(401).json ({ message: 'Not verified' });
@@ -17,8 +18,9 @@ module.exports = (req, res, next) => {
         next();
       }
     })
-
+  
   } else {
+    console.log("no token")
     res.status(400).json({ message: 'No token provided' });
   }
 };
