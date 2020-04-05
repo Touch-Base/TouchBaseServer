cb42aeeabe7fc10b930ab5c36315801c8ab1aca9
anthonyvigz <anthonyvigz@gmail.com>
2020-04-05 17:54:29 -0400

    added favorite column to jobs

--- data/migrations/20200209124213_jobs_table.js
+++ data/migrations/20200209124213_jobs_table.js
@@ -1,5 +1,5 @@
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable("jobs", tbl => {
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable("jobs", (tbl) => {
     tbl.increments("id");
     tbl.string("position").notNullable(); // Position Name
     tbl.string("company").notNullable(); // Company Name
@@ -10,21 +10,18 @@ exports.up = function(knex, Promise) {
         "Connection",
         "Job Website",
         "Company Site",
-        "Other"
+        "Other",
       ])
       .notNullable();
     tbl.timestamp("appDate"); // Application Date
     tbl.text("notes"); // Description of the job/interview notes
     tbl.text("color"); // Color of the job card background
+    tbl.boolean("favorite").defaultTo(false); // Checks to see if the jobs is a favorite
     tbl.boolean("interview").defaultTo(false); // Checks if if there is an interview offer
-    tbl
-      .integer("userId")
-      .unsigned()
-      .references("id")
-      .inTable("users"); // Matches the job to the user
+    tbl.integer("userId").unsigned().references("id").inTable("users"); // Matches the job to the user
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTableIfExists("jobs");
 };
