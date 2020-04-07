de6df0d6c47ba632e3005a1defa9072a1e0ef92c
anthonyvigz <anthonyvigz@gmail.com>
2020-04-07 13:47:58 -0400

    added time to event table

--- data/migrations/20200209124251_events_table.js
+++ data/migrations/20200209124251_events_table.js
@@ -1,19 +1,16 @@
-exports.up = function(knex) {
-  return knex.schema.createTable("events", tbl => {
+exports.up = function (knex) {
+  return knex.schema.createTable("events", (tbl) => {
     tbl.increments("id");
     tbl.string("name").notNullable(); // Name of the event
     tbl.string("location").notNullable(); // Event's location
-    tbl.timestamp("date").notNullable(); // Event's date and time
+    tbl.timestamp("date").notNullable(); // Event's date
+    tbl.string("time").notNullable(); // Event's time
     tbl.text("description"); // Event description
     tbl.boolean("attended").defaultTo(false); // Did you attend the event?
-    tbl
-      .integer("userId")
-      .unsigned()
-      .references("id")
-      .inTable("users"); // Matches the event to the user
+    tbl.integer("userId").unsigned().references("id").inTable("users"); // Matches the event to the user
   });
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema.dropTableIfExists("events");
 };
